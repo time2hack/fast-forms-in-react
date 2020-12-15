67cfad76309ab1e0d00a4fc8ad6ad9792b428b72
Pankaj Patel <pankaj@time2hack.com>
2020-12-15 23:10:10 +0100

    🚏 link tutorial in demo

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <h1>React Fast Forms</h1>
+      <small>
+        Read <a href='https://time2hack.com/form-with-react-html5-validations/'>Building Fast Forms in React with HTML5 & Validations</a>
+        for detailed explanation
+      </small>
       <hr />
       <Form />
     </div>
